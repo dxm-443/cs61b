5094982dcf6cc6f8bcff86cc472a674270123f35
duxiaoman <duxiaoman@sjtu.edu.cn>
2022-03-13 17:25:55 +0800

    modified OffByOne and passed the test

---
+++ proj1b/OffByOne.java
@@ -0,0 +1,11 @@
+
+public class OffByOne implements CharacterComparator{
+    @Override
+    public boolean equalChars(char x, char y){
+        if ( y - x == 1){
+            return true;
+        }
+        return false;
+    }
+
+}
